f63eb8df4d03b34a2489fc1e0d327c2a3aa47a0c
Ekin Koc <ekin@eknkc.com>
2013-12-12 03:04:21 +0200

    Remove unnecessary test code & version bump

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
 module.exports = require("./lib/minwq.js");
-
-var r = new (module.exports)();
-
-r.pop({
-  queue: "anan",
-  data: { a: 1 },
-  ttl: 10000
-}, function (err, job) {
-  if (err) return next(err);
-  if (job) {
-    r.remove({
-      queue: "anan",
-      id: job.id
-    }, console.log)
-  }
-})
